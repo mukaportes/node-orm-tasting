ea69b5da7e721eca10bdd3bf5e2798e1203ff5c0
Murilo Portescheller <mport.dev@gmail.com>
2019-05-01 00:59:37 -0300

    Created ormFactory and node commands

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const setupORM = require('./config/orm-factory');
 
 const app = express();
 
 app.port = process.env.PORT || 3000;
 
 try {
-    await app.use(bodyParser.json());
-    await app.use(bodyParser.urlencoded({ extended: true }));
-    await app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
 
-    app.listen(app.port, setupORM(process.env.ORM));
+    app.listen(app.port, setupORM());
     console.log(`App listening on port ${app.port}`);
 } catch (error) {
     console.log(`App failed to listen ${app.port}. Error: `, error);
